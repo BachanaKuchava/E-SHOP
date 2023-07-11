6bd2252542ddb8b3ed17c45c18ba17e648101d45
bachana kuchava <kuchavabachana@gmail.com>
2023-07-11 06:25:52 +0400

    contactPage

---
+++ src/pages/Contact/Contact.tsx
@@ -0,0 +1,155 @@
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import Breadcrumbs from "../../components/pageProps/Breadcrumbs";
+import { useTranslation } from 'react-i18next';
+import i18n from "../../i18next";
+
+
+const Contact: React.FC = () => {
+  const location = useLocation();
+  const [prevLocation, setPrevLocation] = useState("");
+  // useEffect(() => {
+  //   setPrevLocation(location.state.data);
+  // }, [location]);
+  const { t } = useTranslation();
+  function handleClick(lang:any) {  
+    i18n.changeLanguage(lang);
+  }
+
+  const [clientName, setClientName] = useState("");
+  const [email, setEmail] = useState("");
+  const [messages, setMessages] = useState("");
+
+  // ========== Error Messages Start here ============
+  const [errClientName, setErrClientName] = useState("");
+  const [errEmail, setErrEmail] = useState("");
+  const [errMessages, setErrMessages] = useState("");
+  // ========== Error Messages End here ==============
+  const [successMsg, setSuccessMsg] = useState("");
+
+  const handleName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setClientName(e.target.value);
+    setErrClientName("");
+  };
+
+  const handleEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+    setErrEmail("");
+  };
+
+  const handleMessages = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMessages(e.target.value);
+    setErrMessages("");
+  };
+
+  // ================= Email Validation start here =============
+  const EmailValidation = (email: string) => {
+    return String(email)
+      .toLowerCase()
+      .match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i);
+  };
+  // ================= Email Validation End here ===============
+
+  const handlePost = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!clientName) {
+      setErrClientName("Enter your Name");
+    }
+    if (!email) {
+      setErrEmail("Enter your Email");
+    } else {
+      if (!EmailValidation(email)) {
+        setErrEmail("Enter a Valid Email");
+      }
+    }
+    if (!messages) {
+      setErrMessages("Enter your Messages");
+    }
+    if (clientName && email && EmailValidation(email) && messages) {
+      setSuccessMsg(
+        `Thank you dear ${clientName}, Your messages has been received successfully. Futher details will sent to you by your email at ${email}.`
+      );
+    }
+
+  };
+
+  return (
+    <div className="max-w-container mx-auto px-4">
+      <Breadcrumbs title={t("Contact.1")} prevLocation={prevLocation} />
+      {successMsg ? (
+        <p className="pb-20 w-96 font-medium text-green-500">{successMsg}</p>
+      ) : (
+        <form className="pb-20">
+          <h1 className="font-titleFont font-semibold text-3xl">
+            {t("Fill up a Form.1")}
+          </h1>
+          <div className="w-[500px] h-auto py-6 flex flex-col gap-6">
+            <div>
+              <p className="text-base font-titleFont font-semibold px-2">
+               {t("Full Name.1")}
+              </p>
+              <input
+                onChange={handleName}
+                value={clientName}
+                className="w-full py-1 border-b-2 px-2 text-base font-medium placeholder:font-normal placeholder:text-sm outline-none focus-within:border-primeColor"
+                type="text"
+                placeholder={t("Enter your name here.1")}
+              />
+              {errClientName && (
+                <p className="text-red-500 text-sm font-titleFont font-semibold mt-1 px-2 flex items-center gap-1">
+                  <span className="text-sm italic font-bold">!</span>
+                  {errClientName}
+                </p>
+              )}
+            </div>
+            <div>
+              <p className="text-base font-titleFont font-semibold px-2">
+                {t("Your email.1")}
+              </p>
+              <input
+                onChange={handleEmail}
+                value={email}
+                className="w-full py-1 border-b-2 px-2 text-base font-medium placeholder:font-normal placeholder:text-sm outline-none focus-within:border-primeColor"
+                type="email"
+                placeholder={t("Enter your email here.1")}
+              />
+              {errEmail && (
+                <p className="text-red-500 text-sm font-titleFont font-semibold mt-1 px-2 flex items-center gap-1">
+                  <span className="text-sm italic font-bold">!</span>
+                  {errEmail}
+                </p>
+              )}
+            </div>
+            <div>
+              <p className="text-base font-titleFont font-semibold px-2">
+                {t("Messages.1")}
+              </p>
+              <textarea
+                onChange={handleMessages}
+                value={messages}
+                cols={30}
+                rows={3}
+                className="w-full py-1 border-b-2 px-2 text-base font-medium placeholder:font-normal placeholder:text-sm outline-none focus-within:border-primeColor resize-none"
+                placeholder={t("Enter your messages here.1")}
+              ></textarea>
+              {errMessages && (
+                <p className="text-red-500 text-sm font-titleFont font-semibold mt-1 px-2 flex items-center gap-1">
+                  <span className="text-sm italic font-bold">!</span>
+                  {errMessages}
+                </p>
+              )}
+            </div>
+            <button
+              onClick={handlePost}
+              className="w-44 bg-primeColor text-gray-200 h-10 font-titleFont text-base tracking-wide font-semibold hover:bg-black hover:text-white duration-200"
+            >
+              {t("Post.1")}
+            </button>
+          </div>
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default Contact;
