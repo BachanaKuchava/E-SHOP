d6ac7e4561d4d57af09e045c6582698b6451631f
bachana kuchava <kuchavabachana@gmail.com>
2023-07-11 06:42:52 +0400

    adminPanelView

---
+++ src/components/adminPanel/MainPanel.tsx
@@ -0,0 +1,178 @@
+import React, { useState } from 'react'
+import Search from './panelComponents/Search'
+import DeleteProduct from './panelComponents/DeleteProduct';
+import { CreateProduct } from './panelComponents/CreateProduct';
+import { ChangeProduct } from './panelComponents/UpdateProduct';
+import { TECollapse } from "tw-elements-react";
+interface Props {}
+
+function MainPanel(props: Props) {
+    const [clicked, setClicked] = useState(false);
+    const [activeElement, setActiveElement] = useState("");
+    const {} = props
+    const handleClick = (value: string) => {
+        setClicked(true);
+        if (value === activeElement) {
+          setActiveElement("");
+        } else {
+          setActiveElement(value);
+        }
+      };
+
+    return (
+        <>
+        <div className='w-full  flex justify-center '>
+           <Search /> 
+        </div>
+        
+    
+
+<div id="accordionExample">
+        <div className="rounded-t-lg border border-neutral-200 bg-white dark:border-neutral-600 dark:bg-neutral-800">
+          <h2 className="mb-0" id="headingOne">
+            <button
+              className={`${
+                activeElement === "element1" &&
+                `text-primary [box-shadow:inset_0_-1px_0_rgba(229,231,235)] dark:!text-primary-400 dark:[box-shadow:inset_0_-1px_0_rgba(75,85,99)]`
+              } group relative flex w-full items-center rounded-t-[15px] border-0 bg-white px-5 py-4 text-left text-base text-neutral-800  transition [overflow-anchor:none] hover:z-[2] focus:z-[3] focus:outline-none dark:bg-neutral-800 dark:text-white`}
+              type="button"
+              onClick={() => handleClick("element1")}
+              aria-expanded="false"
+              aria-controls="collapseOne"
+            >
+              Create Product
+              <span
+                className={`${
+                  activeElement === "element1"
+                    ? `rotate-[-180deg] -mr-1`
+                    : `rotate-0 fill-[#212529]  dark:fill-white`
+                } ml-auto h-5 w-5 shrink-0 fill-[#336dec] transition-transform duration-200 ease-in-out motion-reduce:transition-none dark:fill-blue-300`}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="h-6 w-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                  />
+                </svg>
+              </span>
+            </button>
+          </h2>
+          <TECollapse
+            show={activeElement === "element1"}
+            className="!mt-0 !rounded-b-none !shadow-none"
+          >
+            <div className="px-5 py-4 scrollba">
+            <CreateProduct />
+            </div>
+          </TECollapse>
+        </div>
+      </div>
+      <div className="border border-t-0 border-neutral-200 bg-white dark:border-neutral-600 dark:bg-neutral-800">
+        <h2 className="mb-0" id="headingTwo">
+          <button
+            className={`${
+              activeElement === "element2"
+                ? `text-primary [box-shadow:inset_0_-1px_0_rgba(229,231,235)] dark:!text-primary-400 dark:[box-shadow:inset_0_-1px_0_rgba(75,85,99)]`
+                : `transition-none rounded-b-[15px]`
+            } group relative flex w-full items-center rounded-t-[15px] border-0 bg-white px-5 py-4 text-left text-base text-neutral-800 transition [overflow-anchor:none] hover:z-[2] focus:z-[3] focus:outline-none dark:bg-neutral-800 dark:text-white`}
+            type="button"
+            onClick={() => handleClick("element2")}
+            aria-expanded="true"
+            aria-controls="collapseOne"
+          >
+            Update Product
+            <span
+              className={`${
+                activeElement === "element2"
+                  ? `rotate-[-180deg] -mr-1`
+                  : `rotate-0 fill-[#212529] dark:fill-white`
+              } ml-auto h-5 w-5 shrink-0 fill-[#336dec] transition-transform duration-200 ease-in-out motion-reduce:transition-none dark:fill-blue-300`}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="h-6 w-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                />
+              </svg>
+            </span>
+          </button>
+        </h2>
+        <TECollapse
+          show={activeElement === "element2"}
+          className="!mt-0 !rounded-b-none !shadow-none"
+        >
+          <div className="px-5 py-4 scrollbar h-[500px]">
+          <ChangeProduct />
+          </div>
+        </TECollapse>
+      </div>
+      <div className="rounded-b-lg border border-t-0 border-neutral-200 bg-white dark:border-neutral-600 dark:bg-neutral-800">
+        <h2 className="accordion-header mb-0" id="headingThree">
+          <button
+            className={`${
+              activeElement === "element3"
+                ? `text-primary [box-shadow:inset_0_-1px_0_rgba(229,231,235)] dark:!text-primary-400 dark:[box-shadow:inset_0_-1px_0_rgba(75,85,99)]`
+                : `transition-none rounded-b-[15px]`
+            } group relative flex w-full items-center rounded-t-[15px] border-0 bg-white px-5 py-4 text-left text-base text-neutral-800 transition [overflow-anchor:none] hover:z-[2] focus:z-[3] focus:outline-none dark:bg-neutral-800 dark:text-white`}
+            type="button"
+            onClick={() => handleClick("element3")}
+            aria-expanded="true"
+            aria-controls="collapseOne"
+          >
+            Delete Product
+            <span
+              className={`${
+                activeElement === "element3"
+                  ? `rotate-[-180deg] -mr-1`
+                  : `rotate-0 fill-[#212529] dark:fill-white`
+              } ml-auto h-5 w-5 shrink-0 fill-[#336dec] transition-transform duration-200 ease-in-out motion-reduce:transition-none dark:fill-blue-300`}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="h-6 w-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                />
+              </svg>
+            </span>
+          </button>
+        </h2>
+        <TECollapse
+          show={activeElement === "element3"}
+          className="!mt-0 !shadow-none"
+        >
+          <div className="px-5 py-4">
+          <DeleteProduct />
+          </div>
+        </TECollapse>
+      </div>
+     
+        </>
+        
+    )
+}
+
+export default MainPanel
---
+++ src/components/adminPanel/panelComponents/DeleteProduct.tsx
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import React, { useState } from 'react'
+
+interface Props { }
+
+function CreateProduct(props: Props) {
+    const { } = props
+    const [productId, setProductId] = useState('')
+    async function handleDeleteProduct() {
+        try {
+            const resp = await axios.delete(`http://localhost:3001/products/${productId}`, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("ACCESSTOKEN")}`
+                }
+            })
+        setProductId('');
+
+        } catch (error) {
+            console.log(error);
+
+        }
+    }
+    return (
+        <>
+            {/* <label>ID</label>
+            <input type='text' value={productId} onChange={(e) => setProductId(e.target.value)} />
+            <button onClick={() => handleDeleteProduct()}>Delete</button> */}
+
+            <div className="mb-6">
+                <label htmlFor="default-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Enter ID</label>
+                <input type="text"  value={productId} onChange={(e) => setProductId(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                <br />
+                <button type="button" onClick={() => handleDeleteProduct()} className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Delete</button>
+                
+            </div>
+
+
+        </>
+    )
+}
+
+export default CreateProduct
