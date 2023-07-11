83f5bf6941499ca5fc5946f9fe980e8ebf0e8e4e
bachana kuchava <kuchavabachana@gmail.com>
2023-07-11 05:37:07 +0400

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,85 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+  createRoutesFromElements,
+  Route,
+  Routes,
+  ScrollRestoration,
+  BrowserRouter,
+} from "react-router-dom";
+import Footer from "./components/home/Footer/Footer";
+import FooterBottom from "./components/home/Footer/FooterBottom";
+import Header from "./components/home/Header/Header";
+import HeaderBottom from "./components/home/Header/HeaderBottom";
+import SpecialCase from "./components/SpecialCase/SpecialCase";
+import About from "./pages/About/About";
+import SignIn from "./pages/Account/SignIn";
+import SignUp from "./pages/Account/SignUp";
+import Cart from "./pages/Cart/Cart";
+import Contact from "./pages/Contact/Contact";
+import Home from "./pages/Home/Home";
+import Journal from "./pages/Journal/Journal";
+import Offer from "./pages/Offer/Offer";
+import Payment from "./pages/payment/Payment";
+import ProductDetails from "./pages/ProductDetails/ProductDetails";
+import Shop from "./pages/Shop/Shop";
+import MainPanel from "./components/adminPanel/MainPanel";
+import { BsArrowRepeat } from "react-icons/bs";
+import { useContext } from "react";
+import { AuthContext } from "./auth/context/AuthContext";
+import { CurrentUserContext } from "./auth/provider/CurrentUserProvider/CurrentUserProvider";
+import Profile from "./pages/profile/Profile";
+import SearchView from "./components/searchView/SearchView";
 
 
-
-
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <HeaderBottom />
+      <SpecialCase />
+      <Outlet />
+      <Footer />
+      <FooterBottom />
+    </div>
+  );
+};
 function App() {
-
+  const { status } = useContext(AuthContext);
+  const { currentUser } = useContext(CurrentUserContext);
   return (
     <div className="font-bodyFont">
-     
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/shop/:value" element={<Shop />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/journal" element={<Journal />} />
+            <Route path="/offer" element={<Offer />} />
+            <Route path="/product/:id" element={<ProductDetails />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/paymentgateway" element={<Payment />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="*" element={<Home />} />
+            <Route path="/searchView/:value" element={<SearchView />} />
+            {
+              currentUser.user_role === "ADMIN" && (
+                <><Route path="/panel" element={<MainPanel />} /></>
+              )
+            }
+
+            {
+              status === "authorized" && (
+                <><Route path="/profile" element={<Profile />} /></>
+              )
+            }
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
+import { PersistGate } from "redux-persist/integration/react";
+import "slick-carousel/slick/slick.css";
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
 import "./index.css";
 import App from "./App";
-
+import './i18next';
+import { AuthProvider } from './auth/provider/AuthProvider';
+import { CurrentUserProvider } from './auth/provider/CurrentUserProvider/CurrentUserProvider';
+import { CartProvider } from 'react-use-cart'
 
 
 
@@ -10,11 +17,21 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-<>
-<App />
-</>
-              
+  <Suspense fallback={(<div>...Loading</div>)}>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <CurrentUserProvider>
+          <AuthProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
 
+
+          </AuthProvider>
+        </CurrentUserProvider>
+      </PersistGate>
+    </Provider>
+  </Suspense>
 
 );
 
